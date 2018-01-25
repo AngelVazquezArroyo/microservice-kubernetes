80453ae177fe0c8b00730ecbc0f66cbc96fd887c
Joerg Mueller <joerg.mueller@innoq.com>
2018-01-25 12:10:21 +0100

    There was a trace of apache left in the remove-script.

--- microservice-kubernetes-demo/kubernetes-remove.sh
+++ microservice-kubernetes-demo/kubernetes-remove.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 kubectl delete configmap startpage-content
 kubectl delete -f startpage/startpage.yaml
-kubectl delete -f apache/apache.yaml
 kubectl delete -f microservice-kubernetes-demo-catalog/catalog.yaml
 kubectl delete -f microservice-kubernetes-demo-customer/customer.yaml
 kubectl delete -f microservice-kubernetes-demo-order/order.yaml
